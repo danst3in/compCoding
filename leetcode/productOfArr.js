bf0c1d398a784bdac6e9ad7d49f175809f40f9cd
danst3in <50937807+danst3in@users.noreply.github.com>
2020-04-16 10:42:13 -0700

    small refactor productofarr

--- leetcode/productOfArr.js
+++ leetcode/productOfArr.js
@@ -50,45 +50,85 @@ Could you solve it with constant space complexity? (The output array does not co
 //   return resultArr;
 // };
 
-// try to create O(n) time solution
+// // try to create O(n) time solution
+// var productExceptSelf = function (nums) {
+//   let prefixArr = Array(nums.length).fill(0);
+//   prefixArr[0] = 1;
+//   let suffixArr = Array(nums.length).fill(0);
+//   suffixArr[suffixArr.length - 1] = 1;
+//   let resultArr = [];
+//
+//   // iterate through the array
+//   // multiply nums[i-1] * prefix[i-1] start at i=1
+//   // set that product to be new value for current position
+//   for (let i = 1; i < nums.length; i++) {
+//     prefixArr[i] = prefixArr[i - 1] * nums[i - 1];
+//   }
+//
+//   // iterate through the array in reverse direction
+//   // multiply nums[i+1] * suffix[i+1] start at i=length-2
+//   // set that product to be new value for current position
+//   for (let j = nums.length - 2; j >= 0; j--) {
+//     suffixArr[j] = suffixArr[j + 1] * nums[j + 1];
+//   }
+//
+//   // iterate through object and multiply all values with same key [i]
+//   for (let i = 0; i < nums.length; i++) {
+//     resultArr[i] = prefixArr[i] * suffixArr[i];
+//   }
+//
+//   // console.log("resultArr: ", resultArr);
+//
+//   return resultArr;
+// };
+//
+
+// try to create O(n) time solution and O(1)space
 var productExceptSelf = function (nums) {
-  let prefixArr = Array(nums.length).fill(0);
-  prefixArr[0] = 1;
-  let suffixArr = Array(nums.length).fill(0);
-  suffixArr[suffixArr.length - 1] = 1;
-  let resultArr = [];
+  let bool = true;
+  nums.forEach((num) => {
+    if (num !== 0) {
+      bool = false;
+    }
+  });
+  if (bool) return nums;
+
+  let resultArr = Array(nums.length).fill(0);
+  resultArr[0] = 1;
+  let temp = 1;
 
   // iterate through the array
   // multiply nums[i-1] * prefix[i-1] start at i=1
   // set that product to be new value for current position
-  for (let i = 1; i < nums.length; i++) {
-    prefixArr[i] = prefixArr[i - 1] * nums[i - 1];
+  for (let i = 0; i < nums.length; i++) {
+    resultArr[i] = temp;
+    // update temp to be product of current product and current array[i]
+    temp = temp * nums[i];
+    // console.log("resultArr,temp, i: ", resultArr, temp, i);
   }
+  //initialize temp to 1 again before iteration from right side
+  temp = 1;
 
   // iterate through the array in reverse direction
-  // multiply nums[i+1] * suffix[i+1] start at i=length-2
-  // set that product to be new value for current position
-  for (let j = nums.length - 2; j >= 0; j--) {
-    suffixArr[j] = suffixArr[j + 1] * nums[j + 1];
+  // multiply nums[i+1] * temp start at i=length-1
+  // set that product to be new value for temp to be current position on next iteration
+  for (let j = nums.length - 1; j >= 0; j--) {
+    resultArr[j] *= temp;
+    // update temp to be product of current product and current array[i]
+    temp = temp * nums[j];
+    // console.log("resultArr, temp, j: ", resultArr, temp, j);
   }
+  //
+  // // iterate through object and multiply all values with same key [i]
+  // for (let i = 0; i < nums.length; i++) {
+  //   resultArr[i] = prefixArr[i] * suffixArr[i];
+  // }
 
-  // iterate through object and multiply all values with different key [i]
-  for (let i = 0; i < nums.length; i++) {
-    let product = 1;
-    for (let [key, val] of Object.entries(store)) {
-      // if (store.hasOwnProperty(key)) {
-      if (+key !== i) {
-        product *= val;
-        // console.log("store inside entries: ", store);
-        // console.log("key:val ", `${key}:${val}`);
-        // console.log("product: ", product);
-        // }
-      }
-    }
-    resultArr.push(product);
-    // console.log("resultArr: ", resultArr);
-  }
+  // console.log("resultArr: ", resultArr);
+
   return resultArr;
 };
 
-console.log(productExceptSelf([1, 2, 3, 4]));
+// console.log(productExceptSelf([1, 2, 3, 4]));
+console.log(productExceptSelf([0, 0]));
+console.log(productExceptSelf([9, 0, -2]));
