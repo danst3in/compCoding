b40560b2c4cbeacc47a0bf6c64a0d11ec130a744
danst3in <50937807+danst3in@users.noreply.github.com>
2020-06-10 18:42:19 -0700

    add DFS inorder solution for validBST

--- leetcode/validBst.js
+++ leetcode/validBst.js
@@ -44,20 +44,59 @@ Explanation: The root node's value is 5 but its right child's value is 4.
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function (root) {
-  const validHelp = (node, low = -Infinity, high = Infinity) => {
-    if (node === null) return true;
+// var isValidBST = function (root) {
+//   const validHelp = (node, low = -Infinity, high = Infinity) => {
+//     if (node === null) return true;
 
-    if (node.val <= low || node.val >= high) return false;
+//     if (node.val <= low || node.val >= high) return false;
 
-    if (!validHelp(node.right, node.val, high)) return false;
-    if (!validHelp(node.left, low, node.val, high)) return false;
+//     if (!validHelp(node.right, node.val, high)) return false;
+//     if (!validHelp(node.left, low, node.val, high)) return false;
 
-    return true;
-  };
-  return validHelp(root);
-};
+//     return true;
+//   };
+//   return validHelp(root);
+// };
 
 // console.log('isValidBST -> root.left', root.left)
 //   console.log('isValidBST -> root.val', root.val)
 //   console.log('isValidBST -> root.right', root.right)
+
+// alternate solution, not working
+// var isValidBST = function (root) {
+//   if (!root) return true;
+//   let val = root.val;
+
+//   const validHelp = (node) => {
+//     let stack = [[node, -Infinity, Infinity]];
+//     while (stack.length) {
+//       let [currentNode, lower, upper] = stack.pop();
+//       if (!currentNode) continue;
+//       val = currentNode.val;
+//       if ((lower != null && val <= lower) || (upper != null && val >= upper))
+//         return false;
+//       stack.push([currentNode.right, val, upper]);
+//       stack.push(currentNode.left, lower, val);
+//     }
+//     return true;
+//   };
+//   return validHelp(root);
+// };
+
+// DFS inorder solution
+var isValidBST = function (root) {
+  let arr = [];
+  const traverse = (node) => {
+    if (!node) return;
+    traverse(node.left);
+    arr.push(node.val);
+    traverse(node.right);
+  };
+
+  traverse(root);
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] >= arr[i + 1]) return false;
+  }
+  return true;
+};
