d15b0dac55c04deb4f40b1452b37d6e197808091
danst3in <50937807+danst3in@users.noreply.github.com>
2025-03-03 16:15:37 -0800

    Add ESLint and Prettier configuration files; optimize isPalindrome function

--- Educative_io-Grokking_Patterns/twoPointers.ts
+++ Educative_io-Grokking_Patterns/twoPointers.ts
@@ -1,14 +1,33 @@
+// Optimized 2nd attempt
 function isPalindrome(s: string) {
-  const workingArr = s.split("");
+	let i = 0;
+	let j = s.length - 1;
 
-  while (workingArr.length > 1) {
-    let char1 = workingArr.shift();
-    let char2 = workingArr.pop();
-    if (char1 !== char2) {
-      return false;
-    }
-  }
-  return true;
+	while (i < j) {
+		if (s[i] !== s[j]) {
+			return false;
+		}
+		i++;
+		j--;
+	}
+	return true;
 }
+
+// isPalindrome("racecar");
+console.log('🚀 ~ isPalindrome("racecar"):', isPalindrome('racecar'));
+console.log('🚀 ~ isPalindrome("kaYak"):', isPalindrome('kaYak'));
+console.log('🚀 ~ isPalindrome("kaYak"):', isPalindrome('kaYak'));
+// function isPalindrome(s: string) {
+//   const workingArr = s.split("");
+
+//   while (workingArr.length > 1) {
+//     let char1 = workingArr.shift();
+//     let char2 = workingArr.pop();
+//     if (char1 !== char2) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
 export { isPalindrome };
