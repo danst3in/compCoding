d1edb7bd665ca2dc6b2393ad9b9dff327c252e49
danst3in <50937807+danst3in@users.noreply.github.com>
2025-03-04 14:41:06 -0800

    Optimize threeSum function to handle cases where the smallest number is greater than zero

--- Grokking_Patterns/twoPointers/3Sum.ts
+++ Grokking_Patterns/twoPointers/3Sum.ts
@@ -1,8 +1,15 @@
 function threeSum(nums: number[]): number[][] {
 	let sortedArr = nums.sort((a, b) => a - b);
 	let result: number[][] = [];
+	// can we assume sorted array will have valid entries?
+	if (sortedArr[0] > 0) {
+		return result; // If the smallest number is greater than 0, no triplet can sum to 0
+	}
 
-	for (let curr = 0; curr < sortedArr.length - 2; curr++) {
+	for (let curr = 0; curr < sortedArr.length - 2 && sortedArr[curr] <= 0; curr++) {
+		if (sortedArr[curr] > 0) {
+			break; // If the smallest number is greater than 0, no triplet can sum to 0
+		}
 		if (curr > 0 && sortedArr[curr] === sortedArr[curr - 1]) {
 			continue; // Skip duplicate elements
 		}
@@ -13,11 +20,11 @@ function threeSum(nums: number[]): number[][] {
 			let currSum = sortedArr[curr] + sortedArr[left] + sortedArr[right];
 			if (currSum === 0) {
 				result.push([sortedArr[curr], sortedArr[left], sortedArr[right]]);
+				left++;
+				right--;
 				while (left < right && sortedArr[left] === sortedArr[left + 1]) left++; // Skip duplicates
 				while (left < right && sortedArr[right] === sortedArr[right - 1])
 					right--; // Skip duplicates
-				left++;
-				right--;
 			} else if (currSum < 0) {
 				left++;
 			} else {
