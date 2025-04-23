66a2fc580cb1a889f64c1db4b78afcbd68a1a02d
danst3in <50937807+danst3in@users.noreply.github.com>
2025-04-23 15:20:04 -0700

    Refactor threeSum function to use target parameter for comparisons; enhance isPalindrome function with additional checks and optimizations; update package.json and tsconfig.json for module support and new compiler options.

--- Grokking_Patterns/twoPointers/3Sum.ts
+++ Grokking_Patterns/twoPointers/3Sum.ts
@@ -1,13 +1,16 @@
-function threeSum(nums: number[]): number[][] {
+// Given an integer array nums, find and return all unique triplets [nums[i], nums[j], nums[k]],
+// where the indexes satisfy i≠j, i≠k, and j≠k, and the sum of the elements nums[i] + nums[j] + nums[k] == 0.
+
+function threeSum(nums: number[], target = 0): number[][] {
 	let sortedArr = nums.sort((a, b) => a - b);
 	let result: number[][] = [];
 	// can we assume sorted array will have valid entries?
-	if (sortedArr[0] > 0) {
+	if (sortedArr[0] > target) {
 		return result; // If the smallest number is greater than 0, no triplet can sum to 0
 	}
 
 	for (let curr = 0; curr < sortedArr.length - 2 && sortedArr[curr] <= 0; curr++) {
-		if (sortedArr[curr] > 0) {
+		if (sortedArr[curr] > target) {
 			break; // If the smallest number is greater than 0, no triplet can sum to 0
 		}
 		if (curr > 0 && sortedArr[curr] === sortedArr[curr - 1]) {
@@ -18,14 +21,14 @@ function threeSum(nums: number[]): number[][] {
 
 		while (left < right) {
 			let currSum = sortedArr[curr] + sortedArr[left] + sortedArr[right];
-			if (currSum === 0) {
+			if (currSum === target) {
 				result.push([sortedArr[curr], sortedArr[left], sortedArr[right]]);
 				left++;
 				right--;
 				while (left < right && sortedArr[left] === sortedArr[left + 1]) left++; // Skip duplicates
 				while (left < right && sortedArr[right] === sortedArr[right - 1])
 					right--; // Skip duplicates
-			} else if (currSum < 0) {
+			} else if (currSum < target) {
 				left++;
 			} else {
 				right--;
--- Grokking_Patterns/twoPointers/palindrome.ts
+++ Grokking_Patterns/twoPointers/palindrome.ts
@@ -1,10 +1,20 @@
-// Optimized 2nd attempt
+// Optimized 3rd attempt
 function isPalindrome(s: string) {
+	if (s.length == 0) {
+		return true;
+	}
+
 	let i = 0;
 	let j = s.length - 1;
 
 	while (i < j) {
-		if (s[i] !== s[j]) {
+		while (i < j && !/[a-zA-Z0-9]/.test(s[i])) {
+			i++;
+		}
+		while (i < j && !/[a-zA-Z0-9]/.test(s[j])) {
+			j--;
+		}
+		if (s[i].toLocaleLowerCase() !== s[j].toLowerCase()) {
 			return false;
 		}
 		i++;
@@ -12,9 +22,32 @@ function isPalindrome(s: string) {
 	}
 	return true;
 }
+// // Optimized 2nd attempt
+// function isPalindrome(s: string) {
+// 	let tempText = s.replace(/[\W_]/g, '').toLocaleLowerCase();
+// 	if (tempText.length == 0) {
+// 		return true;
+// 	}
+
+// 	let i = 0;
+// 	let j = tempText.length - 1;
+
+// 	while (i < j) {
+// 		if (tempText[i] !== tempText[j]) {
+// 			return false;
+// 		}
+// 		i++;
+// 		j--;
+// 	}
+// 	return true;
+// }
 
 console.log('🚀 ~ isPalindrome("racecar"):', isPalindrome('racecar'));
 console.log('🚀 ~ isPalindrome("kaYak"):', isPalindrome('kaYak'));
+console.log(
+	'🚀 ~ isPalindrome("Madam, in Eden, Im Adam"):',
+	isPalindrome('Madam, in Eden, Im Adam'),
+);
 // function isPalindrome(s: string) {
 //   const workingArr = s.split("");
 
