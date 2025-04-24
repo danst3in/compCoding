553867b59f4ca089c7ec8141a99445413c86ff0c
danst3in <50937807+danst3in@users.noreply.github.com>
2025-04-23 17:12:40 -0700

    Refactor sortColors function to use a three-pointer approach for "improved performance"; update console log for testing multiple cases.

--- Grokking_Patterns/twoPointers/sortColors.ts
+++ Grokking_Patterns/twoPointers/sortColors.ts
@@ -3,21 +3,53 @@
 // Sort the array in place so that the elements of the same color are adjacent,
 // with the colors in the order of red, white, and blue.To improve your problem - solving skills, do not utilize the built -in sort function.
 
+// Optimized (?) second attempt with multiple pointers
+// .10ms
+
 function sortColors(colors: number[]) {
-	for (let i = 0; i < colors.length; i++) {
+	let i = 0,
+		red = 0,
+		blue = colors.length - 1;
+	while (i <= blue) {
 		if (colors[i] == 0) {
-			let temp = colors.splice(i, 1);
-			colors.unshift(temp[0]);
-		} else if (colors[i] == 2) {
-			let temp = colors.splice(i, 1);
-			colors.push(temp[0]);
+			[colors[i], colors[red]] = [colors[red], colors[i]];
+			red++;
+			i++;
+		} else if (colors[i] == 1) {
+			i++;
+		} else {
+			[colors[i], colors[blue]] = [colors[blue], colors[i]];
+			blue--;
 		}
 	}
 
 	return colors;
 }
 
+// .09ms
+// function sortColors(colors: number[]) {
+// 	for (let i = 0; i < colors.length; i++) {
+// 		if (colors[i] == 0) {
+// 			let temp = colors.splice(i, 1);
+// 			colors.unshift(temp[0]);
+// 		} else if (colors[i] == 2) {
+// 			let temp = colors.splice(i, 1);
+// 			colors.push(temp[0]);
+// 		}
+// 	}
+
+// 	return colors;
+// }
+
 console.log(
-	'🚀 ~ sortColors.ts:7 ~ sortColors ~ sortColors([0,1,0]):',
-	sortColors([0, 1, 0]),
+	'🚀 ~ sortColors.ts:7 ~ sortColors ~ sortColors([[0, 1, 0],[1, 1, 0, 2],[2, 1, 1, 0, 0],[2, 2, 2, 0, 1, 0],[2, 1, 1, 0, 1, 0, 2]]):',
+	[
+		[0, 1, 0],
+		[1, 1, 0, 2],
+		[2, 1, 1, 0, 0],
+		[2, 2, 2, 0, 1, 0],
+		[2, 1, 1, 0, 1, 0, 2],
+	].forEach((arr) => {
+		console.log(sortColors(arr));
+	}),
 );
